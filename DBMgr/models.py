e264990be598ef724fa7e9c591de043aa038e941
JiHwan Kim <57897955+doram419@users.noreply.github.com>
2024-09-23 15:02:34 +0900

    효원씨가 정리한 클래스 넣음

--- DBMgr/models.py
+++ DBMgr/models.py
@@ -1,12 +1,46 @@
 from pydantic import BaseModel
 from typing import Optional
+from sqlalchemy import Column, String, Integer, Boolean, Float
+# from database import Base
 
 class SearchResult(BaseModel):
+    """
+    검색 결과 저장되는 클래스, VO
+    """
     title: str
     link: str
-    description: str
-    rating: Optional[float] = None
-    views: Optional[int] = None
-    # TODO: 주소 넣기
-    # description은 큰 특징인데 rdb 저장이 사실상 불가능하지. Vector DB에 넣으면?
-    # 다른 건 Vector DB에 넣을 필요성 있나?
+    address: str
+    reviews : list # 리뷰들, 구글 api는 list(dict)
+    description: Optional[str] = None # 간략 설명
+    rating: Optional[float] = None  # 평점 
+    views: Optional[int] = None     # 조회수
+    price_level: Optional[int] = None   # 가격대, 구글이 제공하는대로 0~4로 구분
+    """
+    0 Free
+    1 Inexpensive
+    2 Moderate
+    3 Expensive
+    4 Very Expensive
+    """
+    serves_beer: Optional[bool] = None
+    serves_wine: Optional[bool] = None
+    serves_breakfast: Optional[bool] = None
+    serves_brunch: Optional[bool] = None
+    serves_lunch: Optional[bool] = None
+    serves_dinner: Optional[bool] = None
+    serves_vegetarian_food: Optional[bool] = None
+    takeout: Optional[bool] = None
+
+
+# TODO: rdb에 적재
+# class Restaurant(Base):
+#     __tablename__ = 'restaurants'
+
+#     id = Column(Integer, primary_key=True, index=True)
+#     name = Column(String, index=True)
+#     address = Column(String)
+#     price_level = Column(Integer, nullable=True)
+#     rating = Column(Float, nullable=True) 
+
+# class instanceData():
+# TODO: 갱신이 자주 필요한 데이터 모음, 이름 검색으로 바로 가져오기? or google_id로 가져오기
