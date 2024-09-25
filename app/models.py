7d1a88ed59e2dcf51073e1da91bba83bee244569
yeaeun123 <yeaeun0125@gmail.com>
2024-09-25 14:27:29 +0900

    로그인 회원가입 구현

--- app/models.py
+++ app/models.py
@@ -1,5 +1,15 @@
 from pydantic import BaseModel
 from typing import Optional
+from sqlalchemy import Column, Integer, String
+from app.database import Base
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(255), unique=True, nullable=False)
+    password_hash = Column(String(255), nullable=False)
+
 
 class SearchResult(BaseModel):
     title: str
