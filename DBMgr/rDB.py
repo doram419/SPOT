1f5cb8866ae795e42e8ec5d2749c2d9e2be2aa40
hyowonzzang <shw2657@naver.com>
2024-09-25 15:07:54 +0900

    db연결 생성 및 테이블 생성
    성공.

--- DBMgr/rDB.py
+++ DBMgr/rDB.py
@@ -1,21 +1,7 @@
 import pymysql
-
+from DBMgr.connetion import create_connection
 from DBMgr.summarizer import summarize_reviews
 from models import SearchResult
-import pymysql
-
-def create_connection():
-    """
-    MySQL 데이터베이스 연결을 생성하는 함수
-    """
-    return pymysql.connect(
-        host='localhost',
-        user='root',
-        password='@rlawlgid121',
-        database='spot',
-        charset='utf8mb4',
-        cursorclass=pymysql.cursors.DictCursor
-    )
 
 def saveToRDB(data: SearchResult):
     """
@@ -28,7 +14,7 @@ def saveToRDB(data: SearchResult):
             # 리뷰를 요약
             summary = summarize_reviews(data.title, data.reviews)
 
-            # 데이터 삽입 SQL 쿼리: 요약된 내용을 description에 저장
+            # 데이터 삽입 SQL 쿼리
             sql = """
             INSERT INTO restaurant 
             (title, description, adr_address, international_phone_number)
@@ -46,21 +32,3 @@ def saveToRDB(data: SearchResult):
 
     finally:
         connection.close()
-        print(cursor.lastrowid)
-
-
-    """
-    MySQL DB에 크롤링한 데이터를 저장하고 생성된 pk를 반환해주는 함수
-
-    저장하는 데이터
-    - 가게명, 리뷰, 주소, 국제 전화번호
-    반환 값 
-    - pk : int
-    """
-
-    # TODO:MySQL에 데이터 생성해서 넣기, 예외처리
-    global temp_pk
-    temp_pk += 1
-    return temp_pk
-
-
