bb32f973f40002cd7abd02d378460e7792ab8c99
yeaeun123 <yeaeun0125@gmail.com>
2024-10-01 16:27:34 +0900

    db 고쳐야할듯..........

--- DBMgr/crawling/google_service.py
+++ DBMgr/crawling/google_service.py
@@ -27,21 +27,21 @@ def fetch_top_restaurants_nearby(query: str = "검색어", region: str = "지역
         place_id = place.get('place_id', 'None')
         place_details = gmaps.place(place_id=place_id, language='ko',
                                     fields=['name', 'url', 'vicinity', 'rating',
-                                            'user_ratings_total', 'price_level', 'reviews',
-                                            'serves_beer', 'serves_wine',
-                                            'serves_breakfast', 'serves_lunch', 'serves_dinner'])['result']
+                                            'user_ratings_total', 'price_level', 'reviews'])['result']
+                                            # 'serves_beer', 'serves_wine',
+                                            # 'serves_breakfast', 'serves_lunch', 'serves_dinner'])['result']
 
         # 필요한 데이터를 리스트로 이어붙이기 위해 desc 필드에 저장
         description_list = [
             f"주소: {place_details.get('vicinity', '주소 없음')}",
             f"평점: {place_details.get('rating', 0.0)}",
             f"리뷰 수: {place_details.get('user_ratings_total', 0)}",
-            f"가격대: {place_details.get('price_level', '가격 정보 없음')}",
-            f"아침 제공: {'예' if place_details.get('serves_breakfast') else '아니요'}",
-            f"점심 제공: {'예' if place_details.get('serves_lunch') else '아니요'}",
-            f"저녁 제공: {'예' if place_details.get('serves_dinner') else '아니요'}",
-            f"맥주 제공: {'예' if place_details.get('serves_beer') else '아니요'}",
-            f"와인 제공: {'예' if place_details.get('serves_wine') else '아니요'}"
+            f"가격대: {place_details.get('price_level', '가격 정보 없음')}"
+            # f"아침 제공: {'예' if place_details.get('serves_breakfast') else '아니요'}",
+            # f"점심 제공: {'예' if place_details.get('serves_lunch') else '아니요'}",
+            # f"저녁 제공: {'예' if place_details.get('serves_dinner') else '아니요'}",
+            # f"맥주 제공: {'예' if place_details.get('serves_beer') else '아니요'}",
+            # f"와인 제공: {'예' if place_details.get('serves_wine') else '아니요'}"
         ]
 
         # 네이버 블로그에서 해당 식당 이름으로 검색한 데이터 가져오기
